e8504e706396f27f4df0f307ec1a6af848c93064
Ihar Pryshchepau <Ihar_Pryshchepau@epam.com>
2022-11-15 00:36:45 +0100

    Add unit tests

---
+++ UnitTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Xunit;
+
+namespace SAS22
+{
+    public class WFATests
+    {
+        [Theory]
+        [MemberData(nameof(BacktestSessionsTestData))]
+        public void BacktestSessionNegativeTests(BacktestSession backtestSession, string message)
+        {
+           Assert.Throws<Xunit.Sdk.XunitException>(() => WorkflowAuditor.VerifyBacktestSessionToAdd(backtestSession))
+                .Message.Contains(message).Should().BeTrue();          
+        }
+
+        [Fact]
+        public void BacktestSessionHappyPathTest()
+        {
+            Action act = () => WorkflowAuditor.VerifyBacktestSessionToAdd(new BacktestSession
+            {
+                SessionId = 123,
+                TestDate = DateTime.Now.ToString(),
+                Pair = "Test",
+                TestPeriod = 12,
+                TestType = "TestType",
+                SettingsId = 123
+            });
+
+            act.Should().NotThrow<Xunit.Sdk.XunitException>();
+        }
+
+        public static IEnumerable<object[]> BacktestSessionsTestData()
+        {
+            yield return new object[] { new BacktestSession { }, "Session Id must be set <> 0" };
+            yield return new object[] { new BacktestSession { SessionId = 123 }, "TestDate parameter must be of DateTime format"};
+            yield return new object[] { new BacktestSession { SessionId = 123, TestDate = DateTime.Now.ToString()},
+                "Pair parameter must be set" };
+            yield return new object[] { new BacktestSession { SessionId = 123, TestDate = DateTime.Now.ToString(),
+            Pair = "Test"}, "TestPeriod must be set > 0" };
+            yield return new object[] { new BacktestSession { SessionId = 123, TestDate = DateTime.Now.ToString(),
+            Pair = "Test", TestPeriod = 12}, "TestType parameter must be set" };
+            yield return new object[] { new BacktestSession { SessionId = 123, TestDate = DateTime.Now.ToString(),
+            Pair = "Test", TestPeriod = 12, TestType = "TestType"}, "SettingsId must be set <> 0" };            
+        }
+    }
+}
--- WorkflowAuditor.cs
+++ WorkflowAuditor.cs
@@ -6,7 +6,7 @@ namespace SAS22
         public static void VerifyBacktestSessionToAdd(BacktestSession backtestSession)
         {
             backtestSession.SessionId.Should().NotBe(0, because: "Session Id must be set <> 0");
-            DateTime.TryParse(backtestSession.TestDate, out _).Should().BeTrue(because: "TestDate parametr must be of DateTime format");
+            DateTime.TryParse(backtestSession.TestDate, out _).Should().BeTrue(because: "TestDate parameter must be of DateTime format");
             backtestSession.Pair.Should().NotBeNullOrEmpty(because: "Pair parameter must be set");
             backtestSession.TestPeriod.Should().NotBe(0, because: "TestPeriod must be set > 0");
             backtestSession.TestType.Should().NotBeNullOrEmpty(because: "TestType parameter must be set");
